3da81b7178d44af2fe4583d2e9e5ffc9b39f1b7a
wangfuyao <wfytjpu@foxmail.com>
2022-01-30 21:36:04 +0800

    map

---
+++ passwd-al.hs
@@ -0,0 +1,50 @@
+import Data.List
+import System.IO
+import Control.Monad(when)
+import System.Exit
+import System.Environment(getArgs)
+
+main = do
+    
+    args <- getArgs
+
+    
+    when (length args /= 2) $ do
+        putStrLn "Syntax: passwd-al filename uid"
+        exitFailure
+
+    
+    content <- readFile (head args)
+
+    
+    let username = findByUID content (read (args !! 1))
+
+    
+    case username of
+         Just x -> putStrLn x
+         Nothing -> putStrLn "Could not find that UID"
+
+
+findByUID :: String -> Integer -> Maybe String
+findByUID content uid =
+    let al = map parseline . lines $ content
+        in lookup uid al
+
+
+parseline :: String -> (Integer, String)
+parseline input = 
+    let fields = split ':' input
+        in (read (fields !! 2), head fields)
+
+
+split :: Eq a => a -> [a] -> [[a]]
+
+
+split _ [] = [[]]
+split delimiter str =
+    let         
+        (before, remainder) = span (/= delimiter) str
+        in before : case remainder of
+                         [] -> []
+                         x -> 
+                              split delimiter (tail x)
